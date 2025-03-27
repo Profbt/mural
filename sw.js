c98bd550525c4b528d5575cf91cb6ac10c2a3857
Professor Bruno Carvalho <101364708+Profbt@users.noreply.github.com>
2025-03-26 23:26:15 -0300

    ttt

--- sw.js
+++ sw.js
@@ -1,20 +1,6 @@
-// Cache simples da página shell
-const CACHE_NAME = 'ponto-shell-v1';
-const SHELL_URLS = ['/index.html'];
-
+// Apenas para habilitar instalação como PWA
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(SHELL_URLS))
-});
-
-// Intercepta todas as navegações
-self.addEventListener('fetch', (event) => {
-    if (event.request.mode === 'navigate') {
-        // Redireciona para o site oficial SEM alterar a URL
-        event.respondWith(Response.redirect(
-            'https://www.registropontoseed.pr.gov.br', 
-            307 // Status de redirecionamento temporário
-        ));
-    }
+  event.waitUntil(
+      caches.open('ponto-shell').then(cache => cache.add('/index.html'))
+  );
 });
